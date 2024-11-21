c67e5b00c9b3a17020092e88b230b8e413760516
Raihannn15 <reyhanmardiansyah5@gmail.com>
2024-11-21 12:24:45 +0700

    Setengah Jalan

---
+++ Cerita-Kopi/img/src/app.js
@@ -0,0 +1,16 @@
+// document.addEventListener("alpine:init", () => {
+//   Alpine.data("products", () => ({
+//     items: [
+//       { id: 1, name: "Kenangan Latte", products: "1.jpg", price: 28000 },
+//       { id: 2, name: "Kisah Matcha", products: "2.png", price: 20000 },
+//       { id: 3, name: "Sejuknya Cerita", products: "3.jpg", price: 10000 },
+//       { id: 4, name: "Sejuta Espresso", products: "8.jpg", price: 23000 },
+//       { id: 5, name: "Aromanya Rindu", products: "9.jpg", price: 15000 },
+//       { id: 6, name: "Nostalgia Nasi Goreng", products: "4.jpg", price: 20000 },
+//       { id: 7, name: "Memori Croissant", products: "5.jpg", price: 13000 },
+//       { id: 8, name: "Cerita Waffle", products: "7.jpg", price: 10000 },
+//       { id: 9, name: "Kisah Manis Gelatoe", products: "6.png", price: 13000 },
+//       { id: 10, name: "Rasa yang Terukir", products: "10.jpg", price: 9000 },
+//     ],
+//   }));
+// });
